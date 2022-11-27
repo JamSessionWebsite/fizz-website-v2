7fb3a97a51e9b46e980ee40dedaf4f3fb798e4fa
spencerkasper <spener.kasper@gmail.com>
2022-11-27 17:44:24 -0600

    * This should look even better on mobile I think as well as add some barebones

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,20 +1,89 @@
-import React from 'react';
-import {Layout, Menu, Skeleton} from "antd";
+import React, {Fragment} from 'react';
+import {Button, Image, Layout, Menu, Skeleton} from "antd";
 import Sider from "antd/es/layout/Sider";
 import JamSessionRoutes from "../routing/JamSessionRoutes";
 import {ItemType} from "antd/es/menu/hooks/useItems";
+import {FacebookOutlined, InstagramOutlined, MailOutlined, YoutubeOutlined} from "@ant-design/icons";
+import {Footer} from "antd/es/layout/layout";
 
 const {Content, Header} = Layout;
-
+const DISABLED_FEATURE = true;
 const AppLayout = ({}) => {
+    const navigateInNewTab = (url) => {
+        window.open(url, '_blank').focus();
+    }
+
+    const populateEmail = () => {
+        window.location.href = `mailto:fizzthebandofficial@gmail.com?subject=Hi FIZZ!`
+    }
 
     return (
-        <Layout style={{minHeight: '100vh'}}>
-            <Content className={`site-layout-background`}>
-                <div className={`site-layout-background`} style={{padding: 24, minHeight: 360}}>
+        <Layout className={`home-page-background`} style={{minHeight: '100vh'}}>
+            <Header className={'app-header'}>
+                <div className={'fizz-image-container'}>
+                    <Image
+                        height={'200px'}
+                        preview={false}
+                        src={'https://audio.fizztheband.com/images/9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d/fizz-soda-red-blue-transparent-bg-256-512.png'}
+                    />
+                </div>
+                {!DISABLED_FEATURE ?
+                    <div className={'flex-row'}>
+                        <div className={'button-container'}>
+                            <Button
+                                ghost>
+                                About Us
+                            </Button>
+                        </div>
+                        <div className={'button-container'}>
+                            <Button
+                                ghost>
+                                Videos
+                            </Button>
+                        </div>
+                    </div> :
+                    <div/>}
+            </Header>
+            <Content>
+                <div style={{padding: 24, minHeight: 360}}>
                     <JamSessionRoutes/>
                 </div>
             </Content>
+            <Footer className={'app-footer'}>
+                <div className={'button-container'}>
+                    <Button
+                        ghost
+                        size={'large'}
+                        onClick={() => populateEmail()}
+                        icon={<MailOutlined/>}
+                    ></Button>
+                </div>
+                <div className={'button-container'}>
+                    <Button
+                        ghost
+                        size={'large'}
+                        onClick={() => navigateInNewTab(`https://www.youtube.com/channel/UCCYlcZuQdCE2gD3k9jsTRJw`)}
+                        icon={<YoutubeOutlined/>}
+                    >
+                    </Button>
+                </div>
+                <div className={'button-container'}>
+                    <Button
+                        ghost
+                        size={'large'}
+                        onClick={() => navigateInNewTab('https://www.instagram.com/fizz.band/')}
+                        icon={<InstagramOutlined/>}
+                    ></Button>
+                </div>
+                <div className={'button-container'}>
+                    <Button
+                        ghost
+                        size={'large'}
+                        onClick={() => navigateInNewTab('https://www.facebook.com/Fizzthebandofficial')}
+                        icon={<FacebookOutlined/>}
+                    ></Button>
+                </div>
+            </Footer>
         </Layout>
     )
 };
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,63 +6,13 @@ import YouTube from "react-youtube";
 const {Title, Text} = Typography;
 
 const HomePage = () => {
-    const navigateInNewTab = (url) => {
-        window.open(url, '_blank').focus();
-    }
-
-    const populateEmail = () => {
-        window.location.href = `mailto:fizzthebandofficial@gmail.com?subject=Hi FIZZ!`
-    }
 
     return (
-        <div className={'home-page-background'}>
-            <div className={'flex-row full-width header-row'}>
-                <Image
-                    preview={false}
-                    width={'30%'}
-                    src={'https://audio.fizztheband.com/images/9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d/fizz-logo-pop-label-transparent.png'}/>
-            </div>
-            <div className={'full-width youtube-container'}>
-                <YouTube
-                    videoId={'Po3-1mDTm7I'}
-                    style={{maxWidth: '640px', overflowX: 'scroll'}}
-                ></YouTube>
-            </div>
-            <div className={'flex-row full-width app-footer'}>
-                <div className={'button-container'}>
-                    <Button
-                        ghost
-                        size={'large'}
-                        onClick={() => populateEmail()}
-                        icon={<MailOutlined/>}
-                    ></Button>
-                </div>
-                <div className={'button-container'}>
-                    <Button
-                        ghost
-                        size={'large'}
-                        onClick={() => navigateInNewTab(`https://www.youtube.com/channel/UCCYlcZuQdCE2gD3k9jsTRJw`)}
-                        icon={<YoutubeOutlined/>}
-                    >
-                    </Button>
-                </div>
-                <div className={'button-container'}>
-                    <Button
-                        ghost
-                        size={'large'}
-                        onClick={() => navigateInNewTab('https://www.instagram.com/fizz.band/')}
-                        icon={<InstagramOutlined/>}
-                    ></Button>
-                </div>
-                <div className={'button-container'}>
-                    <Button
-                        ghost
-                        size={'large'}
-                        onClick={() => navigateInNewTab('https://www.facebook.com/Fizzthebandofficial')}
-                        icon={<FacebookOutlined/>}
-                    ></Button>
-                </div>
-            </div>
+        <div className={'full-width youtube-container'}>
+            <YouTube
+                videoId={'Po3-1mDTm7I'}
+                opts={{width: '296', height: '394'}}
+            ></YouTube>
         </div>
     )
 };
