797046b4472b094993800763a70e3cd8e8450ef3
spencerkasper <spener.kasper@gmail.com>
2022-11-27 14:04:02 -0600

    * Add some basic text

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -10,7 +10,12 @@ const HomePage = () => {
                 <Image
                     preview={false}
                     width={'30%'}
-                    src={'https://audio.fizztheband.com/images/9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d/fizz-logo-pop-label-transparent.png'}></Image>
+                    src={'https://audio.fizztheband.com/images/9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d/fizz-logo-pop-label-transparent.png'}/>
+            </div>
+            <div className={'flex-row full-width main-content-row'}>
+                <Text
+                    style={{fontSize: '32px'}}
+                    className={'white-text'}>Hi! We are a funky pop band from Chicago called FIZZ.  Our website is in the process of being built so please check back soon for more content!</Text>
             </div>
         </div>
     )
