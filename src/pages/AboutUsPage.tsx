23ac371d24f3e89ea65903328d71bdada1783c83
spencerkasper <spener.kasper@gmail.com>
2022-11-28 23:28:07 -0600

    * Add padding to about us page.

--- src/pages/AboutUsPage.tsx
+++ src/pages/AboutUsPage.tsx
@@ -14,31 +14,33 @@ const AboutUsPage = () => {
                       content='FIZZ is a pop/funk/ band in the Chicago area that has been playing together for
                                 many years.  We have also gone under the name Elise and the Police in the past.'></meta>
             </Helmet>
-            <Paragraph className={'flex-row white-text'}>
-                Many members of FIZZ have been playing music together for over 10 years. We have played
-                shows under the name Elise and the Police at House of Blues, Bottom Lounge, and more. We
-                have also played multiple shows under the name FIZZ in the North Shore area including winning
-                the Battle of the Bands at Northwestern University. This allowed us to open for Dillo Day; a
-                music festival with artists such as Hippo Campus and A$AP Ferg performing.
-            </Paragraph>
-            <Paragraph className={'flex-row white-text'}>
-                Currently, we have been working on writing new music and are actively looking for gigs in the Chicago
-                area. FIZZ is made up of 2 vocalists, guitar, bass, drums, trumpet, alto sax, and trombone. We play
-                funky pop
-                covers as well as an assortment of original music.
-            </Paragraph>
-            <Paragraph className={'flex-row full-width space-between white-text'}>
-                <div>
-                    Our 2 lead vocalists also perform solo work under the names Morgan Buckley and Sun Queen. You can
-                    check
-                    out the links to their work on our LinkTree:
-                </div>
-                <div/>
-            </Paragraph>
-            <Paragraph className={'flex-row full-width space-between'}>
-                <a target={'_blank'} href={'https://linktr.ee/fizz.band'}>https://linktr.ee/fizz.band</a>
-                <div/>
-            </Paragraph>
+            <div className={'about-us-text-container'}>
+                <Paragraph className={'about-us-text'}>
+                    Many members of FIZZ have been playing music together for over 10 years. We have played
+                    shows under the name Elise and the Police at House of Blues, Bottom Lounge, and more. We
+                    have also played multiple shows under the name FIZZ in the North Shore area including winning
+                    the Battle of the Bands at Northwestern University. This allowed us to open for Dillo Day; a
+                    music festival with artists such as Hippo Campus and A$AP Ferg performing.
+                </Paragraph>
+                <Paragraph className={'about-us-text'}>
+                    Currently, we have been working on writing new music and are actively looking for gigs in the Chicago
+                    area. FIZZ is made up of 2 vocalists, guitar, bass, drums, trumpet, alto sax, and trombone. We play
+                    funky pop
+                    covers as well as an assortment of original music.
+                </Paragraph>
+                <Paragraph className={'flex-row full-width space-between white-text'}>
+                    <div>
+                        Our 2 lead vocalists also perform solo work under the names Morgan Buckley and Sun Queen. You can
+                        check
+                        out the links to their work on our LinkTree:
+                    </div>
+                    <div/>
+                </Paragraph>
+                <Paragraph className={'flex-row full-width space-between'}>
+                    <a target={'_blank'} href={'https://linktr.ee/fizz.band'}>https://linktr.ee/fizz.band</a>
+                    <div/>
+                </Paragraph>
+            </div>
         </div>
     );
 }
