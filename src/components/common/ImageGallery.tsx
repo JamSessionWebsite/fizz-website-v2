5d211664524e6dea5dd019b3a14d93a4b2bd979a
spencerkasper <spener.kasper@gmail.com>
2022-11-29 15:29:04 -0600

    * More fixing image gallery

--- src/components/common/ImageGallery.tsx
+++ src/components/common/ImageGallery.tsx
@@ -1,12 +1,40 @@
 import React, {useEffect, useState} from 'react';
 import {Button, Card, Image as ImageAntd, Typography} from "antd";
 import {CaretLeftOutlined, CaretRightOutlined} from "@ant-design/icons";
+
 const {Text} = Typography;
 
 const ImageGallery = ({images, title = null}) => {
+    const [imageDimensions, setImageDimensions] = useState({width: 0, height: 0});
+
+    function getBrowserWidth() {
+        return Math.max(
+            document.body.scrollWidth,
+            document.documentElement.scrollWidth,
+            document.body.offsetWidth,
+            document.documentElement.offsetWidth,
+            document.documentElement.clientWidth
+        );
+    }
+
+    const getMaxWidth = (ratio) => {
+        if (getBrowserWidth() <= 760) {
+            return 264;
+        }
+        if (ratio < 1) {
+            return 296;
+        }
+        return 400;
+    }
+
+    useEffect(() => {
+        window.onresize = () => {
+            setMaxWidth(getMaxWidth(imageDimensions.width/imageDimensions.height));
+        }
+    }, [imageDimensions]);
     const [visible, setVisible] = useState(false);
     const [currentlyVisibleImageIndex, setCurrentlyVisibleImageIndex] = useState(0);
-    const [imageDimensions, setImageDimensions] = useState({width: 0, height: 0});
+    const [maxWidth, setMaxWidth] = useState(getMaxWidth(imageDimensions.width/imageDimensions.height));
 
     useEffect(() => {
         onImageChange(images[currentlyVisibleImageIndex].src);
@@ -14,6 +42,7 @@ const ImageGallery = ({images, title = null}) => {
 
     function setHeightAndWidthOfImage() {
         setImageDimensions({height: this.height, width: this.width});
+        setMaxWidth(getMaxWidth(this.width/this.height));
     }
 
     function onImageChange(imgPath) {
@@ -22,28 +51,42 @@ const ImageGallery = ({images, title = null}) => {
         myImage.src = imgPath;
     }
 
-    const maxWidth = 264;
-    const ratio = imageDimensions.width / imageDimensions.height;
-    const calculatedHeight = maxWidth / ratio;
+    const calculatedHeight = maxWidth / (imageDimensions.width / imageDimensions.height);
     const height = `${calculatedHeight}px`;
+    const getTopPixel = () => {
+        const ratio = imageDimensions.width / imageDimensions.height;
+        if(getBrowserWidth() <= 760) {
+            if(ratio < 1) {
+                return calculatedHeight - 52;
+            }
+            return calculatedHeight + 48;
+        }
+        if(ratio < 1) {
+            return calculatedHeight - 64;
+        }
+        return calculatedHeight + 24;
+    }
     return (
         <Card title={title ? title : null}>
             <div className={'flex-row full-width'}>
                 <Text className={'gallery-description-text'}>
-                    This is an assortment of photos of FIZZ playing at various shows!  If you want to see any image
+                    This is an assortment of photos of FIZZ playing at various shows! If you want to see any image
                     more clearly, just click it and you can view it in full screen mode.
                 </Text>
             </div>
             <div className={'image-gallery-container'}>
                 <div className={'display-image-container'}>
-                    <ImageAntd
-                        alt={images[currentlyVisibleImageIndex].description}
-                        width={maxWidth}
-                        height={height}
-                        src={images[currentlyVisibleImageIndex].src}
-                        onClick={() => setVisible(true)}
-                        preview={{visible: false}}/>
-                    <div style={{top: `${ratio < 1 ? calculatedHeight - 88 : calculatedHeight - 84}px`}} className={'image-description'}>
+                    <div className={'display-image'}>
+                        <ImageAntd
+                            alt={images[currentlyVisibleImageIndex].description}
+                            width={maxWidth}
+                            height={height}
+                            src={images[currentlyVisibleImageIndex].src}
+                            onClick={() => setVisible(true)}
+                            preview={{visible: false}}/>
+                    </div>
+                    <div style={{top: `${getTopPixel()}px`}}
+                         className={'image-description'}>
                         {images[currentlyVisibleImageIndex].description}
                     </div>
                 </div>
