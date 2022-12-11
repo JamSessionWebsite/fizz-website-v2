2d0e38a5b1b0d9e828aa7f1489c2fb5b7c8d28fb
spencerkasper <spener.kasper@gmail.com>
2022-12-11 11:02:21 -0600

    * Add fizztheband.com?

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-      domains: ['audio.fizztheband.com']
-  }
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        domains: [
+            'audio.fizztheband.com',
+            'fizztheband.com'
+        ]
+    }
 }
 
 module.exports = nextConfig;
