4b4ccd1a2cc9a18be61ba32b1ab7c010f81c1f22
spencerkasper <spener.kasper@gmail.com>
2022-12-18 16:12:07 -0600

    * Extract photos to band config.

--- band-config.ts
+++ band-config.ts
@@ -8,6 +8,13 @@ export interface Logo {
     alt: string;
 }
 
+export interface Photo {
+    src: string;
+    description: string;
+    alt?: string;
+    groupId?: string;
+}
+
 export interface BandWebsiteConfig {
     mainTitle: string;
     bandName: string;
@@ -15,7 +22,10 @@ export interface BandWebsiteConfig {
     logo: Logo;
     primaryColor?: string;
     socialMedia: SocialMedia[];
+    photos: Photo[];
 }
+
+const IMAGES_PATH = 'https://audio.fizztheband.com/images/fizz-website/';
 
 export const BAND_WEBSITE_CONFIG: BandWebsiteConfig = {
     mainTitle: 'FIZZ: A Local Chicago Pop/Funk/Indie Band',
@@ -31,5 +41,54 @@ export const BAND_WEBSITE_CONFIG: BandWebsiteConfig = {
         {platform: 'instagram', pageSrc: 'https://www.instagram.com/fizz.band/'},
         {platform: 'tiktok', pageSrc: 'https://www.tiktok.com/@fizz.band'},
         {platform: 'facebook', pageSrc: 'https://www.facebook.com/Fizzthebandofficial'},
+    ],
+    photos: [
+        {
+            description: 'FIZZ performs "Something Cosmic" written by Sun Queen (feat. FIZZ) at Bookclub Chicago.',
+            src: `${IMAGES_PATH}fizz-performs-something-cosmic.jpg`
+        },
+        {
+            description: 'Morgan Buckley, one of the lead singers of FIZZ, singing at Bookclub Chicago in late 2022.',
+            src: `${IMAGES_PATH}morgan-buckley-close-up.jpg`
+        },
+        {
+            description: 'Mary aka Sun Queen, one of the lead singers of FIZZ, singing at Bookclub Chicago in late 2022.',
+            src: `${IMAGES_PATH}sun-queen-plays-something-cosmic.jpg`
+        },
+        {description: 'Spencer, drummer of FIZZ, playing at Bookclub Chicago.', src: `${IMAGES_PATH}drums-playing-live.jpg`},
+        {
+            description: 'Alec, bassist of FIZZ, playing his Fender Jazz Bass at Bookclub Chicago.',
+            src: `${IMAGES_PATH}bass-playing-live-fizz.jpg`
+        },
+        {
+            description: 'Patrick, guitarist of FIZZ, playing Disco Ulysses by Vulfpeck at Bookclub Chicago.',
+            src: `${IMAGES_PATH}guitar-playing-live.jpg`
+        },
+        {description: 'Sam, trumpet player of FIZZ, takes a solo.', src: `${IMAGES_PATH}trumpet-solo-live.jpg`},
+        {description: 'Close up of the drums and rhythm guitar locking in.', src: `${IMAGES_PATH}drums-and-guitar.jpg`},
+        {
+            description: 'Full horn section plays in unison to a funky pop song.',
+            src: `${IMAGES_PATH}horn-section-of-fizz-bookclub-chicago.jpg`
+        },
+        {
+            description: 'Close up of the drummer of FIZZ playing at Bookclub Chicago.',
+            src: `${IMAGES_PATH}close-up-drummer-of-fizz.jpg`
+        },
+        {
+            description: 'A close up of DJ, the trombone player of FIZZ, taking a solo.',
+            src: `${IMAGES_PATH}trombone-solo-live.jpg`
+        },
+        {
+            description: 'The rhythm section of FIZZ playing a cover of Disco Ulysses by Vulfpeck.',
+            src: `${IMAGES_PATH}fizz-rhythm-section-live.jpg`
+        },
+        {
+            description: 'Ethan, saxophone player of FIZZ, taking a solo at Bookclub Chicago.',
+            src: `${IMAGES_PATH}sax-solo-live.jpg`
+        },
+        {
+            description: 'Setting up before the gig at Bookclub Chicago in late 2022.',
+            src: `${IMAGES_PATH}gig-setup-for-fizz.jpg`
+        },
     ],
 }
--- pages/HomePage.tsx
+++ pages/HomePage.tsx
@@ -4,59 +4,7 @@ import Head from "next/head";
 import {BAND_WEBSITE_CONFIG} from "../band-config";
 const ImageGallery = dynamic(() => import("../components/common/ImageGallery"));
 
-const PATH = 'https://audio.fizztheband.com/images/fizz-website/';
-
 const HomePage = () => {
-    const pictures = [
-        {
-            description: 'FIZZ performs "Something Cosmic" written by Sun Queen (feat. FIZZ) at Bookclub Chicago.',
-            src: `${PATH}fizz-performs-something-cosmic.jpg`
-        },
-        {
-            description: 'Morgan Buckley, one of the lead singers of FIZZ, singing at Bookclub Chicago in late 2022.',
-            src: `${PATH}morgan-buckley-close-up.jpg`
-        },
-        {
-            description: 'Mary aka Sun Queen, one of the lead singers of FIZZ, singing at Bookclub Chicago in late 2022.',
-            src: `${PATH}sun-queen-plays-something-cosmic.jpg`
-        },
-        {description: 'Spencer, drummer of FIZZ, playing at Bookclub Chicago.', src: `${PATH}drums-playing-live.jpg`},
-        {
-            description: 'Alec, bassist of FIZZ, playing his Fender Jazz Bass at Bookclub Chicago.',
-            src: `${PATH}bass-playing-live-fizz.jpg`
-        },
-        {
-            description: 'Patrick, guitarist of FIZZ, playing Disco Ulysses by Vulfpeck at Bookclub Chicago.',
-            src: `${PATH}guitar-playing-live.jpg`
-        },
-        {description: 'Sam, trumpet player of FIZZ, takes a solo.', src: `${PATH}trumpet-solo-live.jpg`},
-        {description: 'Close up of the drums and rhythm guitar locking in.', src: `${PATH}drums-and-guitar.jpg`},
-        {
-            description: 'Full horn section plays in unison to a funky pop song.',
-            src: `${PATH}horn-section-of-fizz-bookclub-chicago.jpg`
-        },
-        {
-            description: 'Close up of the drummer of FIZZ playing at Bookclub Chicago.',
-            src: `${PATH}close-up-drummer-of-fizz.jpg`
-        },
-        {
-            description: 'A close up of DJ, the trombone player of FIZZ, taking a solo.',
-            src: `${PATH}trombone-solo-live.jpg`
-        },
-        {
-            description: 'The rhythm section of FIZZ playing a cover of Disco Ulysses by Vulfpeck.',
-            src: `${PATH}fizz-rhythm-section-live.jpg`
-        },
-        {
-            description: 'Ethan, saxophone player of FIZZ, taking a solo at Bookclub Chicago.',
-            src: `${PATH}sax-solo-live.jpg`
-        },
-        {
-            description: 'Setting up before the gig at Bookclub Chicago in late 2022.',
-            src: `${PATH}gig-setup-for-fizz.jpg`
-        },
-    ];
-
     return (
         <div className={'full-width youtube-container'}>
             <Head>
@@ -68,7 +16,7 @@ const HomePage = () => {
             <div className={'flex-row image-gallery-container'}>
                 <ImageGallery
                     title={'FIZZ Image Gallery'}
-                    images={pictures}/>
+                    images={BAND_WEBSITE_CONFIG.photos}/>
             </div>
         </div>
     )
