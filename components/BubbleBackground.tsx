d4654081ef97ebc3e33a4946a694e58c42d324f7
spencerkasper <spener.kasper@gmail.com>
2023-09-15 18:15:09 -0500

    * Handle mobile better

--- components/BubbleBackground.tsx
+++ components/BubbleBackground.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import useBreakpoint from "use-breakpoint";
+import {BREAKPOINTS} from "./common/CollapsibleHeader";
 
-const NUMBER_OF_BUBBLES = 75;
 export const getRandomInt = (min, max) => {
     const minCeiling = Math.ceil(min);
     const maxFloor = Math.floor(max);
     return Math.floor(Math.random() * (maxFloor - minCeiling + 1)) + minCeiling;
 };
 const BubbleBackground = () => {
+    const {minWidth} = useBreakpoint(BREAKPOINTS, 'mobile');
+    const NUMBER_OF_BUBBLES = minWidth === 0 ? 25 : 75;
     return (
         <div style={{top: '0', bottom: '100vh', left: '0', right: '100vw', position: 'fixed'}}>
             {Array.from(Array(NUMBER_OF_BUBBLES).keys()).map(x => <div
